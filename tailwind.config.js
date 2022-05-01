4f250af6f9d41a2349a8f9fc32d0df932dab8a24
Koubovi laptop <jankouba93@gmail.com>
2022-05-01 09:13:16 +0200

    styles config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,23 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        desktop: "1440px",
+        mobile: "375px",
+      },
+      colors: {
+        neutral: {
+          slightlyTransparentWhite: "hsla(0, 0%, 100%, 0.75)",
+          slightlyTransparentWhite: "hsla(0, 0%, 100%, 0.6)",
+        },
+        primary: {
+          veryDarkBlue: "hsl(233, 47%, 7%)",
+          darkDesaturatedBlue: "hsl(244, 38%, 16%)",
+          softViolet: "hsl(277, 64%, 61%)",
+        },
+      },
+    },
   },
   plugins: [],
-}
+};
